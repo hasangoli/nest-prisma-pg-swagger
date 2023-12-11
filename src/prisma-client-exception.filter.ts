6589e5c9e52b135554cb4fd6a0dcdcc16c883a91
Hasan Goli <hasangolidev@gmail.com>
2023-12-11 21:03:59 +0330

    Comments added to prisma exception filter

--- src/prisma-client-exception.filter.ts
+++ src/prisma-client-exception.filter.ts
@@ -5,15 +5,27 @@ import { Response } from 'express';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  /**
+   * Handles exceptions thrown by the Prisma client.
+   * @param exception - The Prisma client exception.
+   * @param host - The arguments host object.
+   */
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     console.error(exception.message);
+
+    // Get the response object from the HTTP context.
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+
+    // Remove line breaks from the exception message.
     const message = exception.message.replace(/\n/g, '');
 
     switch (exception.code) {
       case 'P2002': {
+        // If the exception code is P2002, set the status to CONFLICT.
         const status = HttpStatus.CONFLICT;
+
+        // Return a JSON response with the status code and message.
         response.status(status).json({
           statusCode: status,
           message: message,
@@ -21,7 +33,10 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
         break;
       }
       case 'P2025': {
+        // If the exception code is P2025, set the status to NOT_FOUND.
         const status = HttpStatus.NOT_FOUND;
+
+        // Return a JSON response with the status code and message.
         response.status(status).json({
           statusCode: status,
           message: message,
@@ -29,6 +44,7 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
         break;
       }
       default:
+        // If the exception code is not handled, call the parent catch method.
         super.catch(exception, host);
         break;
     }
