5beaec81af1c6421d39a278bc06491e129d885bb
Hasan Goli <hasangolidev@gmail.com>
2023-12-12 22:31:40 +0330

    Added helmet and enabled cors for security

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
 
   app.setGlobalPrefix('api');
 
